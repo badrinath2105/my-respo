a1e824d8a33591ef32177b2135d3f1cf9f402254
badrinath2105 <115928035+badrinath2105@users.noreply.github.com>
2022-10-31 14:54:37 +0530

    Add files via upload

--- ass5.py
+++ ass5.py
@@ -1,4 +1,14 @@
-my_dict={}
-for i in range(97,97+26):
-    my_dict[chr(i)]=i
-    print(my_dict)
+x=input("Enter the string:- ")
+def char(x):
+  u=0
+  l=0
+  for i in x:
+      if i>='a' and i<='z':
+       l+=1
+
+      if i >='A' and i<='Z':
+       u+=1
+
+  print("LowerCase letter in the String",l)
+  print("UpperCase letter in the String",u)
+char(x)
